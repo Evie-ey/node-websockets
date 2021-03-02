575e493ff85cb5e87223a53a997959ad80056df9
Evelyne Namwoyo <namwoyoeve@gmail.com>
2021-03-02 14:20:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,37 +10,6 @@ var uuidv4 = require('uuid').v4;
 let requests = []
 
 app.use(cors());
-const message = {
-  "id": "2efff104-a640-43b6-549b-08d879923d62",
-  "initiator": "Evelyne Namwoyo",
-  "initiatorId": "a8366738-5dad-451b-8e70-f86810d6f037",
-  "initiatorEmail": "evelyne@laboremus.no",
-  "participantId": "6527ae7e-09c6-4c8f-ac2f-a60685efe720",
-  "receivedAt": "2020-10-26T12:33:47.4529644",
-  "submittedAt": "2020-10-26T12:33:47.7311262",
-  "receivedFromNira": "2020-10-26T12:33:48.331357",
-  "billingUpdated": "2020-10-26T12:33:50.0216534",
-  "requestStatus": "Completed",
-  "surname": "NANSIKOMBI",
-  "givenNames": "ROSE",
-  "cardNumber": "000078675",
-  "maskedCardNumber": "00007****",
-  "nin": "CM88456101R55E",
-  "maskedNin": "CM8845********",
-  "dateOfBirth": "2020-10-04T00:00:00",
-  "resultJson": {
-    "matchingStatus": null,
-    "cardStatus": null,
-    "status": "Error",
-    "error": {
-      "code": "320",
-      "message": "Person not found, NIN: CM88456101R55E"
-    },
-    "isError": true,
-    "ninStatus": "Person not found, NIN: CM88456101R55E"
-  }
-}
-
 
 io.on('connection', function (socket) {
   socket.on('event://send-request', function (ninReq) {
